44d35b279971a47d7c319ce5db54bab7081301db
Ankur Lahoti <alahoti@tibco.com>
2019-10-03 20:02:39 +0530

    Adding weather temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = ( latitude , longitude , callback ) => {
            callback( undefined, 
             body.daily.data[0].summary+' It is currently '
             +body.currently.temperature+' degrees out. There is a '
-            +(body.currently.precipProbability*100)+'% chance of rain')
+            +(body.currently.precipProbability*100)+'% chance of rain. '
+            +'The high today is '+ body.daily.data[0].temperatureHigh+' the low todays is '+body.daily.data[0].temperatureLow)
         }
     })
 }
